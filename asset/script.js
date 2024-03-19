baae5c76521a52b63d859b6ac68bf81ac0faf931
AlessioRomano95 <romanalex400@gmail.com>
2024-03-19 17:45:30 +0100

    [ADD] campo di errore - task

--- asset/script.js
+++ asset/script.js
@@ -9,14 +9,33 @@ createApp({
         {
           text: 'Lavare la macchina',
           done: true
-        }
+        },
+        {
+          text: 'Andare dal veterinario',
+          done: true
+        },
+        {
+          text: 'Comprare le medicine',
+          done: true
+        },
+        {
+          text: 'Comprare biglietto aereo',
+          done: true
+        },
       ],
-      newTask:''
+      newTask:'',
+      errorMsg:''
     }
   },
   methods: {
     addTask(){
-      console.log('add')
+      if(this.newTask.length > 5){
+      this.todoList.unshift(this.newTask)
+      this.newTask = ''
+      this.errorMsg = ''
+        }else{
+        this.errorMsg = 'Attenzione! La task deve essere formata da almeno 5 caratteri!'
+        }
     }
   },
 }).mount('#app')
